f5148e62e62a2fe53f8a29df4354c2fb81bb8fa8
lish01 <lish01@gettysburg.edu>
2018-11-08 17:32:24 -0500

    Update GetInfo.java

--- GetInfo.java
+++ GetInfo.java
@@ -4,11 +4,20 @@ import java.net.*;
 import java.util.*;
 
 import javafx.application.*;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+//import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.layout.VBox;
+import javafx.scene.text.Font; 
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight; 
+import javafx.scene.text.Text;
+//import javafx.stage.Popup;
 //import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -152,6 +161,56 @@ public class GetInfo extends Application{
 		
 		return result;
 	}
+	
+	public static void showStage(){
+		
+		//Creating a Text_Example object  
+		Text text1 = new Text("Set SensorTrack TMCC ID and Action Command");     
+		
+		//Setting font to the text 
+		text1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+		
+		//setting the position of the text 
+		text1.setX(50); 
+		text1.setY(50);      
+		
+		//underlining the text     
+		text1.setUnderline(true);
+		
+		Text text2 = new Text("1. Press and hold the PROGRAM button. \n" + 
+		"2. The Program LED will begin to blink about 2 times a second. Release the button. \n" +
+		"3. On your Lionel Remote (CAB-1L or Legacy CAB-2) press the ACC button (Accessory mode). \n" +
+		"4. Now enter the two numberical digits of the ID you wish to set. For ID 1-9, use a leading zero (ex. '03' for IO three). \n" +
+		"If you have more than one SensorTrack, be sure to give each a unique ID. \n" +
+		"5. Press the SET button. Notice that the Program LED flashes quickly, then goes back to blinking. \n" +
+		"You 've now set the TMCC Accessory ID. Next you must choose an Action Command to complete the programming sequence. \n" +
+		"6. Press AUX1 on your remote, then the digit 0. This assigns the behaviro 'no action'. \n" +
+		"You'll notice that the Program LED flashes once more and goes dark, indicating the programming sequence is complete.");
+		
+		text2.setX(50);
+		text2.setY(80);
+		
+		Text text3 = new Text("  To test the Sensor Track, run a Legacy locomotive over the track section and watch the Program LED. \n" +
+		"It should illuminate for about a second as the engine passes.");
+		
+		text3.setX(50);
+		text3.setY(250);
+		
+		Stage newStage = new Stage();
+		Group root = new Group();
+		root.getChildren().addAll(text1, text2, text3);
+//		VBox comp = new VBox();
+//		TextField nameField = new TextField("Name");
+//		TextField phoneNumber = new TextField("Phone Number");
+//		comp.getChildren().add(nameField);
+//		comp.getChildren().add(phoneNumber);
+		
+		
+
+		Scene stageScene = new Scene(root, 1000, 1000);
+		newStage.setScene(stageScene);
+		newStage.show();
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception 
@@ -168,10 +227,26 @@ public class GetInfo extends Application{
 		
 		ArrayList<Button> buttons = new ArrayList<Button>();
 		
+		Text text = new Text("Check for your components' ID");
+		
+		text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+		text.setUnderline(true);
+		
 		Button btn_1 = new Button();
 		int text_1 = HexToDec(wifiID.get(0));
 		btn_1.setText("wifi " + text_1);
 		buttons.add(btn_1);
+		
+		
+	    btn_1.setOnAction(new EventHandler<ActionEvent>() 
+	    {
+	    	
+	    	@Override
+	    	public void handle(ActionEvent event) 
+	    	{	
+	    		showStage();	
+	    	}
+	    });
 		
 		for (int i = 0; i < switchID.size(); i++)
 		{
@@ -179,6 +254,16 @@ public class GetInfo extends Application{
 			int text_2 = HexToDec(switchID.get(i));
 		    btn_2.setText("switch " + text_2);
 		    buttons.add(btn_2);
+		    
+		    btn_2.setOnAction(new EventHandler<ActionEvent>() 
+		    {
+		    	
+		    	@Override
+		    	public void handle(ActionEvent event) 
+		    	{	
+		    		showStage();	
+		    	}
+		    });
 		}
 		
 		for (int i = 0; i < sensorID.size(); i++)
@@ -187,33 +272,34 @@ public class GetInfo extends Application{
 			int text_3 = HexToDec(sensorID.get(i));
 		    btn_3.setText("sensor " + text_3);
 		    buttons.add(btn_3);
+		    
+		    btn_3.setOnAction(new EventHandler<ActionEvent>() 
+		    {
+		    	
+		    	@Override
+		    	public void handle(ActionEvent event) 
+		    	{	
+		    		showStage();	
+		    	}
+		    });
 		}
-//	    btn.setOnAction(new EventHandler<ActionEvent>() 
-//	    {
-//	    	
-//	    	@Override
-//	    	public void handle(ActionEvent event) 
-//	    	{
-//	    		System.out.println("Hello World!");
-//	    	}
-//	    });
+
 	    
 	    GridPane root = new GridPane();
 	    
 	    int nRows = buttons.size();
 	    
-	    root.setHgap(nRows);
+	    root.setHgap(nRows + 1);
 	    root.setVgap(3);
 	    //root.setPadding(new Insets(0, 10, 0, 10));
 	    
+	    root.add(text, 2, 0);
+	    
 	    for(int i = 0; i < buttons.size(); i++)
 	    {
-	    	root.add(buttons.get(i), 2, i);
+	    	root.add(buttons.get(i), 2, i+1);
 	    }	    
-
-	    Button check = new Button();
-	    check.setText("check");
-	    root.add(check, 2, buttons.size());
+	    
 	    Scene scene = new Scene(root, 1000, 850);
 	    
 	    primaryStage.setTitle("Check components");
