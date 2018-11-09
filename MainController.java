585b8cfb1ac362c2089e2c45f4c164d3adcbf142
naamirani <43768250+naamirani@users.noreply.github.com>
2018-11-08 19:11:37 -0500

    Add files via upload

---
+++ MainController.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+
+/**
+ * Main controller class for the entire layout.
+ */
+public class MainController {
+
+    /** Holder of a switchable vista. */
+    @FXML
+    private StackPane viewHolder;
+
+    /**
+     * Replaces the vista displayed in the vista holder with a new vista.
+     *
+     * @param node the vista node to be swapped in.
+     */
+    public void setView(Node node) {
+        viewHolder.getChildren().setAll(node);
+    }
+
+}
